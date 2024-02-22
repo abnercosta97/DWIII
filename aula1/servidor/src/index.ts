6279f643ba9a6af94351d687b13ae7d6a3d48007
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-02-22 19:59:40 -0300

    Atualização connection

--- aula1/servidor/src/index.ts
+++ aula1/servidor/src/index.ts
@@ -1,19 +1,18 @@
 import express from "express";
+import routes from './routes';
 import dotenv from "dotenv";
-import routes from "./routes";
-import conectar from "./models/conection";
+import connect from "./models/connection";
 dotenv.config();
-
-const PORT = process.env.PORT;
-
-const app = express();
+// será usado 3000 se a variável de ambiente não tiver sido definida
+const PORT = process.env.PORT || 3000;
+const app = express(); // cria o servidor e coloca na variável app
+// suportar parâmetros JSON no body da requisição
 app.use(express.json());
-
-conectar();
-
-app.listen(PORT, () =>
-  console.log(`Rodando na porta ${PORT}`)
-);
-
+// conecta ao MongoDB no início da aplicação
+connect();
+// inicializa o servidor na porta especificada
+app.listen(PORT, () => {
+console.log(`Rodando na porta ${PORT}`);
+});
+// define a rota para o pacote /routes
 app.use(routes);
-//
